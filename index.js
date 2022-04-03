5ddf15e42d2a9a5543ffcbaed4aa1d9611e682bf
Christopher Pravlik <christopherpravlik@Christophers-iMac.local>
2022-04-03 14:10:25 -0600

    added axios

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const axios = require('axios')
 const path = require('path')
 const PORT = process.env.PORT || 3000
 
@@ -6,7 +7,18 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('pages/index'))
+
+app.get('/', (req, res) => {
+    res.render('pages/index')
+})
+
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-app.get('/user', (req, res) => res.render('pages/user'))
+app.get('/user', async(req, res) => {
+
+    user = await axios.get("http://localhost:7071/api/user-trigger?userid=203416")
+
+    console.log(user)
+
+    res.render('pages/user')
+})
